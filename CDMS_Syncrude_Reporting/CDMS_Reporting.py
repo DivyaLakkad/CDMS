e47595edf51abdf4e08ab3af7bf24d7d0ac74efd
DivyaLakkad <47064030+DivyaLakkad@users.noreply.github.com>
2021-05-14 16:44:01 -0600

    Syncrude reporting begins

---
+++ CDMS_Syncrude_Reporting/CDMS_Reporting.py
@@ -0,0 +1,9 @@
+from Functions import login_cdms
+from Functions import get_report
+
+
+if __name__ == '__main__':
+
+    login_cdms('SWPbjaved', 'D1rectorchris', 'D1rectorchris!03')
+
+    get_report('')
