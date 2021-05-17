49923079490182e02d0f2956c35f4fb5532a8b0a
DivyaLakkad <47064030+DivyaLakkad@users.noreply.github.com>
2021-05-17 10:49:25 -0600

    login and get report functions

--- CDMS_Syncrude_Reporting/CDMS_Reporting.py
+++ CDMS_Syncrude_Reporting/CDMS_Reporting.py
@@ -1,9 +1,15 @@
-from Functions import login_cdms
-from Functions import get_report
+from functions import login_cdms
+from functions import get_report
+from functions import browser
 
 
 if __name__ == '__main__':
 
     login_cdms('SWPbjaved', 'D1rectorchris', 'D1rectorchris!03')
 
-    get_report('')
+    # Go to report download page
+    repo_link: str = "https://cdms.exxonmobil.com/reports.aspx"
+    # Report 1 (Regular Swipe Report)
+    browser.get(repo_link)
+
+    get_report('Reconcile and Payment Status Detailed Listing (Excel)', '2021/04/01', '2021/05/01')
